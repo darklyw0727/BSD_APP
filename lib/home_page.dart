dc16f12c332faebdb93e69b385881bea28a8a455
YuWei Liao <10965122@gm.nfu.edu.tw>
2021-10-14 15:19:10 +0800

    新增藍芽功能

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,12 +1,13 @@
 import 'dart:async';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 
 import './bluetooth_page.dart';
-import './chat_page.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class HomePage extends StatefulWidget {
-  //const HomePage({Key? key}) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -20,11 +21,16 @@ class _HomePageState extends State<HomePage> {
   BluetoothConnection? connection;
   bool get isConnected => (connection?.isConnected ?? false);
 
-  Timer? _bluetoothTimer;
-  bool timerStart = false;
+  //警示燈號
   bool front = false; //前方警示
   bool backRight = false; //右後警示
   bool backLeft = false; //左後警示
+
+  //藍芽資料處理
+  int dataIndex = 5; //資料片段指標
+  String longitude = ''; //經度
+  String latitude = ''; //緯度
+  String mySpeed = ''; //速度
 
   //紅黑顏色變換
   Color changeColor(bool warning){
@@ -35,49 +41,15 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
-  void bluetoothTimer(){
-    _bluetoothTimer = Timer.periodic(Duration(milliseconds: 100), (timer) {
-      if(timerStart){
-        print('0.1sec');
-        setState(() {
-
-        });
-      }
-    });
-  }
-
   @override
-  void initState() {
-    super.initState();
+  void dispose() {
+    // Avoid memory leak (`setState` after dispose) and disconnect
+    if (isConnected) {
+      connection?.dispose();
+      connection = null;
+    }
 
-    // Get current state
-    FlutterBluetoothSerial.instance.state.then((state) {
-      setState(() {
-        _bluetoothState = state;
-      });
-    });
-
-    Future.doWhile(() async {
-      // Wait if adapter not enabled
-      if ((await FlutterBluetoothSerial.instance.isEnabled) ?? false) {
-        return false;
-      }
-      await Future.delayed(Duration(milliseconds: 0xDD));
-      return true;
-    });
-
-    // Listen for futher state changes
-    FlutterBluetoothSerial.instance
-        .onStateChanged()
-        .listen((BluetoothState state) {
-      setState(() {
-        _bluetoothState = state;
-
-        // Discoverable mode is disabled when Bluetooth gets disabled
-        _discoverableTimeoutTimer = null;
-        _discoverableTimeoutSecondsLeft = 0;
-      });
-    });
+    super.dispose();
   }
 
   @override
@@ -96,6 +68,7 @@ class _HomePageState extends State<HomePage> {
                     color:changeColor(front),
                   )
               ),
+              //中間
               SizedBox(
                 height: 20,
                 width: 1000,
@@ -147,11 +120,17 @@ class _HomePageState extends State<HomePage> {
             child: Column(
               children: [
                 //速度
-                Container(
-                  child: Container(
-                    child: isConnected
-                        ?Text('Speed'):Text('fail'),
-                    padding: EdgeInsets.all(55),
+                Container( //外框
+                  child: Container( //內框
+                    child: Container( //文字
+                      alignment: Alignment.center,
+                      child: Text(mySpeed,style: TextStyle(
+                        fontSize: 45,
+                        fontWeight: FontWeight.bold,
+                      ),),
+                      width: 100,
+                    ),
+                    padding: EdgeInsets.all(40),
                     decoration: const BoxDecoration(
                       shape: BoxShape.circle,
                       color: Colors.white,
@@ -186,8 +165,7 @@ class _HomePageState extends State<HomePage> {
                         alignment: Alignment.centerLeft,
                         width: 100,
                         height: 50,
-                        child: selectedDevice == null
-                          ?Text('null'):Text(selectedDevice!.name.toString()),
+                        child: Text(longitude),
                       ),
                     ],
                   ),
@@ -208,7 +186,7 @@ class _HomePageState extends State<HomePage> {
                         alignment: Alignment.centerLeft,
                         width: 100,
                         height: 50,
-                        child: Text('null'),
+                        child: Text(latitude),
                       ),
                     ],
                   ),
@@ -276,14 +254,26 @@ class _HomePageState extends State<HomePage> {
                       connection = _connection;
                       setState(() {
                       });
+
+                      connection!.input!.listen(_printData).onDone(() {
+                        // Example: Detect which side closed the connection
+                        // There should be `isDisconnecting` flag to show are we are (locally)
+                        // in middle of disconnecting process, should be set before calling
+                        // `dispose`, `finish` or `close`, which all causes to disconnect.
+                        // If we except the disconnection, `onDone` should be fired as result.
+                        // If we didn't except this (no flag set), it means closing by remote.
+                        /*if (isDisconnecting) {
+                          print('Disconnecting locally!');
+                        } else {
+                          print('Disconnected remotely!');
+                        }*/
+                        if (this.mounted) {
+                          setState(() {});
+                        }
+                      });
                     });
-                    bluetoothTimer();
                   }else{
                     print('NoSelected !');
-                    //bluetoothTimer();
-                    if(_bluetoothTimer != null){
-                      _bluetoothTimer!.cancel();
-                    }
                   }
                 },
               )
@@ -291,5 +281,98 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
     );
+  }
+
+  void _printData(Uint8List data){
+    //資料長度
+    int countLong = 0;
+    int countLat = 0;
+    int countSpeed = 0;
+
+    //確認資料長度
+    data.forEach((byte) {
+      //不計算換行鍵
+      if(byte != 13){
+        if(byte != 10){
+          //切換資料段指標
+          if(byte == 78 || byte == 83){ //N or S
+            dataIndex = 0;
+          }else if(byte == 69 || byte == 87){ //E or W
+            dataIndex = 1;
+          }else if(byte == 77){ //M
+            dataIndex = 2;
+          }
+
+          //依指標登記資料長度(不計算字母)
+          if(dataIndex == 0){
+            if(byte != 78 && byte != 83){
+              countLong ++;
+            }
+          }else if(dataIndex == 1){
+            if(byte != 69 && byte != 87){
+              countLat ++;
+            }
+          }else if(dataIndex == 2){
+            if(byte != 77){
+              countSpeed ++;
+            }
+          }
+        }
+      }
+    });
+
+    print(countLong);
+    print(countLat);
+    print(countSpeed);
+    print('********' + dataIndex.toString() + '*************');
+
+    //依照資料長度建立Buffer
+    Uint8List longBuffer = Uint8List(countLong);
+    Uint8List latBuffer = Uint8List(countLat);
+    Uint8List speedBuffer = Uint8List(countSpeed);
+
+    //由後至前檢查資料並填進Buffer
+    for(int a = data.length-1 ; a >= 0 ; a--){
+      //跳過換行鍵
+      if(data[a] != 13){
+        if(data[a] != 10){
+          //依照指標與資料長度填入
+          if(dataIndex == 2 && countSpeed > 0){
+            speedBuffer[--countSpeed] = data[a];
+          }else if(dataIndex == 1 && countLat > 0){
+            latBuffer[--countLat] = data[a];
+          }else if(dataIndex == 0 && countLong > 0){
+            longBuffer[--countLong] = data[a];
+          }
+
+          //切換指標
+          if(data[a] == 77){ //M
+            dataIndex = 1;
+          }else if(data[a] == 69 || data[a] == 87) { //E or W
+            dataIndex = 0;
+          }
+        }
+      }
+    }
+
+    //字串轉換
+    setState(() {
+      if(longBuffer.length > 1){
+        longitude = String.fromCharCodes(longBuffer);
+      }
+      if(latBuffer.length > 1){
+        latitude = String.fromCharCodes(latBuffer);
+      }
+      if(speedBuffer.length > 1){
+        mySpeed = String.fromCharCodes(speedBuffer);
+      }
+    });
+
+    print(longitude);
+    print('------------' + longitude.length.toString());
+    print(latitude);
+    print('xxxxxxxxxxxxxxx' + latitude.length.toString());
+    print(mySpeed);
+    print('ooooooooooooooo' + mySpeed.length.toString());
   }
 }
